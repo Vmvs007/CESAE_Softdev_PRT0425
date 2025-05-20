bdc25c8b09432b38b1fa61f63c46e85060c87022
Vitor Santos <vmvs007@gmail.com>
2025-05-20 12:28:09 +0100

    FichasPraticas

---
+++ FichasPraticas/src/FichaPratica07/Ex01.java
@@ -0,0 +1,33 @@
+package FichaPratica07;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Ex01 {
+
+    /**
+     * Função que imprime o conteúdo de um ficheiro na consola
+     * @param caminhoFicheiro Caminho para o ficheiro
+     * @throws FileNotFoundException Caso o ficheiro não exista
+     */
+    public static void imprimirFicheiro(String caminhoFicheiro) throws FileNotFoundException {
+
+        File ficheiro = new File(caminhoFicheiro);
+        Scanner sc = new Scanner(ficheiro);
+
+        while (sc.hasNextLine()) {
+            String linha = sc.nextLine();
+            System.out.println(linha);
+        }
+
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        imprimirFicheiro("Ficheiros/exercicio_01.txt");
+        imprimirFicheiro("Ficheiros/exercicio_01_Alternativa02.txt");
+
+    }
+}
